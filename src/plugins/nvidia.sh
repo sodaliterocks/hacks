52f94bd6d57b75099a8d09292f96647424c61b43
Ducky <ducky@sodalite.rocks>
2022-02-02 19:08:17 +0000

    🐞 nvidia: fix uninstall not working

--- src/plugins/nvidia.sh
+++ src/plugins/nvidia.sh
@@ -14,35 +14,34 @@ driver_packages=""
 kernel_args="rd.driver.blacklist=nouveau --append=modprobe.blacklist=nouveau --append=nvidia-drm.modeset=1"
 
 function main() {
+    [[ -z $driver ]] && driver="latest"
+        
+    case $driver in
+        "latest")
+            driver_packages="akmod-nvidia xorg-x11-drv-nvidia-cuda"
+            ;;
+        "470")
+            driver_packages="akmod-nvidia-470xx xorg-x11-drv-nvidia-470xx-cuda"
+            ;;
+        "390")
+            driver_packages="akmod-nvidia-390xx xorg-x11-drv-nvidia-390xx-cuda"
+            ;;
+        "340")
+            driver_packages="akmod-nvidia-340xx xorg-x11-drv-nvidia-340xx-cuda"
+            ;;
+        *)
+            die_message="Invalid --driver selection, choose either:\n"
+            die_message+=" - latest: current GeForce/Quadro/Tesla\n"
+            die_message+=" - 470: legacy GeForce 600/700\n"
+            die_message+=" - 390: legacy GeForce 400/500\n"
+            die_message+=" - 340: legacy GeForce 8/9/200/300 (EOL)"
+
+            die $die_message
+            ;;
+    esac
+
     if [[ $install == "true" ]]; then
         has_run="true"
-        
-        [[ -z $driver ]] && driver="latest"
-        
-        case $driver in
-            "latest")
-                driver_packages="akmod-nvidia xorg-x11-drv-nvidia-cuda"
-                ;;
-            "470")
-                driver_packages="akmod-nvidia-470xx xorg-x11-drv-nvidia-470xx-cuda"
-                ;;
-            "390")
-                driver_packages="akmod-nvidia-390xx xorg-x11-drv-nvidia-390xx-cuda"
-                ;;
-            "340")
-                driver_packages="akmod-nvidia-340xx xorg-x11-drv-nvidia-340xx-cuda"
-                ;;
-            *)
-                die_message="Invalid --driver selection, choose either:\n"
-                die_message+=" - latest: current GeForce/Quadro/Tesla\n"
-                die_message+=" - 470: legacy GeForce 600/700\n"
-                die_message+=" - 390: legacy GeForce 400/500\n"
-                die_message+=" - 340: legacy GeForce 8/9/200/300 (EOL)"
-
-                die $die_message
-                ;;
-        esac
-        
         invoke_install
     fi
     
