001f934421164c77ecfeb6ffafb4712ec75c1847
Ducky <ducky@sodalite.rocks>
2022-12-19 21:53:34 +0000

    🆕 migrate: add plugin

---
+++ src/plugins/migrate.sh
@@ -0,0 +1,152 @@
+#!/usr/bin/env bash
+
+_PLUGIN_TITLE="Migrate"
+_PLUGIN_DESCRIPTION="..."
+#_PLUGIN_OPTIONS=(
+#    "setting;s;Panel for GNOME Control Center"
+#)
+_PLUGIN_HIDDEN="true"
+_PLUGIN_ROOT="true"
+
+_installed_apps_file="$(get_confdir)/unattanded-installed-apps"
+
+function install_flatpak_app() {
+    repo=$1
+    app=$2
+    branch=$3
+
+    [[ -z $branch ]] && branch="stable"
+
+    flatpak install --assumeyes --noninteractive --or-update --system $repo $app $branch
+}
+
+function is_flatpak_app_installed() {
+    app=$1
+    repo=$2
+
+    installed="true"
+
+    if [[ -z $repo ]]; then
+        flatpak info --system $1 > /dev/null 2>&1 || installed="false"
+    else
+        flatpak info --system $1 | grep "Origin: $2" > /dev/null 2>&1 || installed="false"
+    fi
+
+    echo $installed
+}
+
+function is_flatpak_repo_installed() {
+    if [[ $(flatpak remotes --columns=url --system | grep "$1") ]]; then
+        echo "true"
+    fi
+}
+
+function update_status() {
+    migrate_status_file="$_pidfile_dir/migrate-status"
+    status="$@"
+
+    if [[ ! -f "$migrate_status_file" ]]; then
+        mkdir -p "$_pidfile_dir"
+        touch "$migrate_status_file"
+    fi
+
+    if [[ "$status" == "" ]]; then
+        rm -f $migrate_status_file
+    else
+        echo "$status"
+        echo "$status" > $migrate_status_file
+    fi
+}
+
+function main() {
+    pid="$(set_pidfile)"
+    core=""
+    run_flatpak_uninstall_unused="false"
+
+    if [[ -f "/usr/lib/sodalite-core" ]]; then
+        core="$(cat /usr/lib/sodalite-core)"
+    else
+        del_pidfile
+        die "Core could not be determined"
+    fi
+
+    touch "$_installed_apps_file"
+
+    if [[ $core == "pantheon" ]]; then
+        update_status "Adding AppCenter remote..."
+        flatpak remote-add \
+            --if-not-exists \
+            --system \
+            --comment="The open source, pay-what-you-want app store from elementary" \
+            --description="Reviewed and curated by elementary to ensure a native, privacy-respecting, and secure experience" \
+            --gpg-import=/usr/share/gnupg/appcenter.gpg \
+            --homepage="https://appcenter.elementary.io/" \
+            --icon="https://flatpak.elementary.io/icon.svg" \
+            --if-not-exists \
+            --title="AppCenter" \
+            appcenter https://flatpak.elementary.io/repo/
+    fi
+
+    apps=(
+        "gnome:fedora:org.gnome.Calculator"
+        "gnome:fedora:org.gnome.Calendar"
+        "gnome:fedora:org.gnome.Characters"
+        "gnome:fedora:org.gnome.Connections"
+        "gnome:fedora:org.gnome.Contacts"
+        "gnome:fedora:org.gnome.Evince"
+        "gnome:fedora:org.gnome.Extensions"
+        "gnome:fedora:org.gnome.FileRoller"
+        "gnome:fedora:org.gnome.Logs"
+        "gnome:fedora:org.gnome.Maps"
+        "gnome:fedora:org.gnome.NatutilusPreviewer"
+        "gnome:fedora:org.gnome.Screenshot"
+        "gnome:fedora:org.gnome.TextEditor"
+        "gnome:fedora:org.gnome.Weather"
+        "gnome:fedora:org.gnome.baobab"
+        "gnome:fedora:org.gnome.clocks"
+        "gnome:fedora:org.gnome.eog"
+        "gnome:fedora:org.gnome.font-viewer"
+        "gnome:fedora:org.gnome.gedit"
+        "pantheon:appcenter:org.gnome.Evince:stable"
+        "pantheon:appcenter:org.gnome.FileRoller:stable"
+        "pantheon:appcenter:io.elementary.calculator:stable"
+        "pantheon:appcenter:io.elementary.camera:stable"
+        "pantheon:appcenter:io.elementary.capnet-assist:stable"
+        "pantheon:appcenter:io.elementary.screenshot:stable"
+        "pantheon:appcenter:io.elementary.tasks:stable"
+        "pantheon:appcenter:io.elementary.videos:stable"
+    )
+
+    for app in "${apps[@]}"
+    do
+        app_core="$(echo "$app" | awk -F':' '{print $1}')"
+        app_repo="$(echo "$app" | awk -F':' '{print $2}')"
+        app_id="$(echo "$app" | awk -F':' '{print $3}')"
+        app_branch="$(echo "$app" | awk -F':' '{print $4}')"
+
+        if [[ "$app_core" == "$core" ]]; then
+            if [[ ! $(grep -Fxq "$app" "$_installed_apps_file") ]]; then
+                update_status "Installing app '$app_id'..."
+                if [[ $(is_flatpak_app_installed "$app_id" "$app_repo") == "false" ]]; then
+                    install_flatpak_app "$app_repo" "$app_id" "$app_branch"
+                    [[ $? == 0 ]] && echo "$app" >> $_installed_apps_file
+                fi
+            fi
+        else
+            if [[ $(is_flatpak_app_installed "$app_id" "$app_repo") == "true" ]]; then
+                update_status "Uninstalling app '$app_id'..."
+                flatpak uninstall --assumeyes --force-remove --noninteractive $app_id
+                [[ $? == 0 ]] && sed -i /$app/d  $_installed_apps_file
+                [[ $run_flatpak_uninstall_unused == "false" ]] && run_flatpak_uninstall_unused="true"
+            fi
+        fi
+    done
+    
+    if [[ $run_flatpak_uninstall_unused == "true" ]]; then
+        update_status "Uninstalling unused apps..."
+        flatpak uninstall --assumeyes --noninteractive --unused
+    fi
+
+    update_status
+    pid="$(del_pidfile)"
+}
